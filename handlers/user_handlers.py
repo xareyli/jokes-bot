fcafa9a7e036d59550c9e24cf3dc0a259aa7fd5b
alexwaiten <alexwaiten4@gmail.com>
2023-06-29 15:58:07 +0300

    feat: add joke command

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,9 +1,22 @@
 from dispatcher import dp
 from aiogram.filters.command import Command
 from aiogram import types
+from jokes import get_random_joke
 
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+
+
+@dp.message(Command("joke"))
+async def cmd_joke(message: types.Message):
+    joke = get_random_joke()
+
+    if joke:
+        await message.answer(joke)
+    else:
+        await message.answer(
+            "Чёт я начал барахлить, напишите пожалуйста моему разработчику"
+        )
