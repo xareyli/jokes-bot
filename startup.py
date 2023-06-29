562fad82c0cf835ca5ad9c98607b8d747ad17294
alexwaiten <alexwaiten4@gmail.com>
2023-06-29 12:38:09 +0300

    chore: add delay between workers start

--- startup.py
+++ startup.py
@@ -2,6 +2,7 @@ from data import get_all_groups, delete_group
 from handlers.bot_handlers import send_anekdot
 from scheduler import schedule_task
 from dispatcher import bot
+import asyncio
 
 
 async def startup_hook():
@@ -14,11 +15,11 @@ async def startup_hook():
         try:
             await bot.get_chat_member(chat_id=group[1], user_id=bot_id)
 
-            print("Running worker")
-
             # исключение не выброшено, бот в группе
             schedule_task(group[1], send_anekdot, group[2], group[1])
+
+            # задержка 3 секунды перед запуском следующего воркера
+            await asyncio.sleep(3)
         except:
-            print("Group deleted")
             # бота удалили
             print(delete_group(group[1]))
